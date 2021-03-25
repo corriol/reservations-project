a40bd6c7d62916ae6da7998c44e379dba2801c8c
corriol <vjorda@gmail.com>
2021-03-25 20:11:15 +0100

    Session manager with redirections

--- index.php
+++ index.php
@@ -1,5 +1,55 @@
-<?php
-declare(strict_types=1);?>
+<?declare(strict_types=1);
+
+// it's a good practise to save the cookie name in a variable to avoid misspelling errors.
+$cookieName = "last_visit_date";
+
+// we get the current cookie value
+$lastVisit = filter_input(INPUT_COOKIE, $cookieName, FILTER_VALIDATE_INT);
+
+// we can also use the coalsece operator
+/* $lastVisit =(int)($_COOKIE[$cookieName] ?? null); //
+// or the traditional isset
+/* if (isset($_COOKIE[$cookieName])) {
+$lastVisit = (int)$_COOKIE[$cookieName];
+} else
+$lastVisit = null;
+*/
+
+// if null we show a welcome message
+if (empty($lastVisit))
+    $message = "Welcome to our reservation system!";
+else
+    $message = "Welcome back, your last visit was on " . date("d/m/Y h:i:s", $lastVisit);
+
+// we register the current time and set the expiration date to the next week.
+setcookie($cookieName, (string)time(), time() + 7 * 24 * 60 * 60);
+
+
+// We turn on the session support
+session_start();
+$confirmationMessage = $_SESSION["message"] ?? "";
+unset($_SESSION["message"]);
+
+$sessionKey = "visits";
+
+// check if is the first visit
+$visits = $_SESSION[$sessionKey]??[];
+
+// if not empty generate a HTML Unordered List
+/*
+if (!empty($visits))
+$messageSession =  "<ul><li>" . implode("</li><li>", array_map(function($v) {
+        return date("d/m/Y h:i:s", $v);
+        }, $visits)) . "</li></ul>";
+else
+$messageSession = "Welcome to our reservation system (session version)!";
+*/
+$_SESSION[$sessionKey][] = time();
+
+
+
+?>
+
 
 <!doctype html>
 <html lang="en">
@@ -8,18 +58,25 @@ declare(strict_types=1);?>
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Reservations</title>
+    <title>Document</title>
 </head>
 <body>
 <nav>
     <ul>
         <li><a href="index.php">Home</a></li>
-        <li><a href="reservations.php">Admin</a></li>
+        <li><a href="reservations.php">Dashboard</a></li>
         <li>Login</li>
         <li>Logout</li>
     </ul>
 </nav>
 <h1>Reservation service</h1>
+<p><?= $message ?></p>
+
+
+<?php if (!empty($confirmationMessage)) :?>
+    <h2>Success!</h2>
+    <p><?= $confirmationMessage ?></p>
+<?php endif ?>
 <p>What do you want to do?</p>
 <ul>
     <li><a href="reservations-create.php">Make a reservation</a></li>
--- reservations-create.php
+++ reservations-create.php
@@ -1,8 +1,25 @@
 <?php
 declare(strict_types=1);
+// it's a good practise to save the cookie name in a variable to avoid misspelling errors.
+$cookieName = "last_used_name";
 
+// we get the current cookie value
+$lastUsedName = filter_input(INPUT_COOKIE, $cookieName);
 
-$errors = [];
+// if empty (null or false, see filter_input for more details) we assign empty string to ensure
+// $lastUsedName is a string.
+if (empty($lastUsedName))
+    $lastUsedName = "";
+
+// start the session management
+session_start();
+
+// get the current values
+$errors = $_SESSION["errors"] ?? [];
+
+// unset values
+unset($_SESSION["errors"]);
+
 try {
     $pdo = new PDO("mysql:host=mysql-server; dbname=reservations", "user_db", "abcd");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -64,15 +81,19 @@ try {
 <h1>Reserve a court</h1>
 
 <?php if (!empty($errors)): ?>
-    <?php foreach ($errors as $error): ?>
-        <p><?= $error ?></p>
-    <?php endforeach; ?>
+    <h2>Error!</h2>
+    <p>There are some errors:</p>
+    <ul>
+        <?php foreach ($errors as $error): ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
+    </ul>
 <?php endif; ?>
 
 <form action="reservations-store.php" method="post">
     <div>
         <label for="name">Nombre:</label>
-        <input type="text" id="name" name="name">
+        <input type="text" id="name" name="name" value="<?= $lastUsedName ?>">
     </div>
     <div>
         <label for="date">Date:</label>
@@ -83,8 +104,8 @@ try {
 
         <?php foreach ($timeslots as $time): ?>
             <label for="ts-<?= $time["id"] ?>"><input type="radio" value="<?= $time["id"] ?>"
-                                                              id="ts-<?= $time["id"] ?>"
-                                                              name="timeslot_id"><?= $time["name"] ?>
+                                                      id="ts-<?= $time["id"] ?>"
+                                                      name="timeslot_id"><?= $time["name"] ?>
             </label>
         <?php endforeach; ?>
 
